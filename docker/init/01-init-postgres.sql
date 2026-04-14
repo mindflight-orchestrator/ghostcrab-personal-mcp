e7a252fbb6b7fd44f04c5ca2c381eafab58511e8
dlamotte <denis@mitambo.com>
2026-04-14 09:56:15 +0200

    remove postgres docker

--- docker/init/01-init-postgres.sql
+++
@@ -1,9 +0,0 @@
--- Create extensions in dependency order (postgres database).
--- roaringbitmap must load before pg_facets / pg_dgraph / pg_pragma (pg_pragma requires roaringbitmap).
-CREATE EXTENSION IF NOT EXISTS pgcrypto CASCADE;
-CREATE EXTENSION IF NOT EXISTS vector;
-CREATE EXTENSION IF NOT EXISTS roaringbitmap;
-CREATE EXTENSION IF NOT EXISTS pg_trgm;
-CREATE EXTENSION IF NOT EXISTS pg_facets;
-CREATE EXTENSION IF NOT EXISTS pg_dgraph;
-CREATE EXTENSION IF NOT EXISTS pg_pragma;
